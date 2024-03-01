f8c99948271de7dcf989bba879c9fd22c3d363fb
Ethan Portman <c0523782@online.camosun.ca>
2024-03-01 09:32:43 -0800

    Initial Commit

---
+++ Treasure_Hunting/Treasure_HuntingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treasure_HuntingApp.swift
+//  Treasure_Hunting
+//
+//  Created by ICS 224 on 2024-03-01.
+//
+
+import SwiftUI
+
+@main
+struct Treasure_HuntingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
