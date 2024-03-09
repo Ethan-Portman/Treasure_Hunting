1006abde68cfae5839af89a88896aeacf1c27c2d
Ethan Portman <c0523782@online.camosun.ca>
2024-03-09 10:43:16 -0800

    Added documentation for the board class

--- Board.swift
+++ Board.swift
@@ -1,21 +1,24 @@
-//
-//  Board.swift
-//  Treasure_Hunting
-//
-//  Created by ICS 224 on 2024-03-08.
-//
-
 import Foundation
 
+/// A class representing a game board for a Treasure Hunting Game.
+/// The Board is a 2D Array of type Tile and is responsible for managing the placement of treasure items.
+/// Treasure Items with the same name are placed adjacent to eachother.
 @Observable class Board {
+    /// The size of the game board.
     let boardSize = 6
-    var tiles: [[Tile]]
-    var itemGroups: [String: [(row: Int, column: Int)]]
     
+    /// A 2D array representing the tiles on the game board.
+    var tiles: [[Tile]]
+    
+    /// A dictionary storing the groups of treasure items on the board.
+    var treasureItemGroups: [String: [(row: Int, column: Int)]]
+    
+    /// Initializes a new instance of the 'Board' Class
+    /// The board starts out empty with no treasure items.
     init() {
         tiles = [[Tile]]()
-        itemGroups = [String: [(row: Int, column: Int)]]()
-        
+        treasureItemGroups = [String: [(row: Int, column: Int)]]()
+
         for _ in 1...boardSize {
             var tileRow = [Tile]()
             for _ in 1...boardSize {
@@ -25,27 +28,113 @@ import Foundation
         }
     }
     
-    subscript(row: Int, column: Int) -> String? {
+    /// Gets or sets the treasure item at the specified row and column on the board.
+    /// - Parameter row: The row of the board.
+    /// - Parameter col: The column of the board
+    /// - Returns: The name of the treasure item at the specified location, or nil if out of bounds
+    subscript(row: Int, col: Int) -> String? {
         get {
-            if (row < 0) || (boardSize <= row) || (column < 0) || (boardSize <= column) {
+            guard (0..<boardSize).contains(row), (0..<boardSize).contains(col) else {
                 return nil
             }
-            return tiles[row][column].item
+            return tiles[row][col].item
         }
         set {
-            if (row < 0) || (boardSize <= row) || (column < 0) || (boardSize <= column) {
+            guard (0..<boardSize).contains(row), (0..<boardSize).contains(col) else {
                 return
             }
-            tiles[row][column].item = newValue ?? ""
+            tiles[row][col].item = newValue ?? ""
             
-            // Add the cell to the item group
             if let newValue = newValue {
-                if itemGroups[newValue] == nil {
-                    itemGroups[newValue] = [(row, column)]
-                } else {
-                    itemGroups[newValue]?.append((row, column))
+                treasureItemGroups[newValue, default: []].append((row, col))
+            }
+        }
+    }
+    
+    /// Populates the board with treasure items.
+    /// - Parameter treasureItems: An array of treasure items to be placed onto the board.
+    public func populateBoard(treasureItems: [TreasureItem]) {
+        let totalItems = treasureItems.reduce(0) { $0 + $1.count }
+        var successfullyPlacedItems = 0
+
+        while (successfullyPlacedItems != totalItems) {
+            resetBoard()
+            successfullyPlacedItems = placeItemsOnBoard(treasureItems: treasureItems)
+        }
+    }
+    
+    /// Resets the board by clearing all tiles and item groups
+    private func resetBoard() {
+        for row in 0..<boardSize {
+            for column in 0..<boardSize {
+                self[row, column] = ""
+            }
+        }
+        treasureItemGroups = [String: [(row: Int, column: Int)]]()
+    }
+
+
+    
+    /// Places treasure items on the board.
+    /// Treasure items with the same name are grouped together so they can be placed adjacently on the board.
+    /// - Parameter treasureItems: An array of treasure items to be placed on the board.
+    /// - Returns: The total number of successfully placed items.
+    private func placeItemsOnBoard(treasureItems: [TreasureItem]) -> Int {
+        var successfullyPlacedItems = 0
+        
+        for treasureItem in treasureItems {
+            if let cell = findEmptyCell() {
+                self[cell.row, cell.column] = treasureItem.title
+                successfullyPlacedItems += 1
+                treasureItemGroups[treasureItem.title, default: []].append(cell)
+            }
+            
+            for _ in 2...treasureItem.count {
+                if let adjacentCell = findAdjacentEmptyCell(itemGroup: treasureItemGroups[treasureItem.title] ?? []) {
+                    self[adjacentCell.row, adjacentCell.column] = treasureItem.title
+                    successfullyPlacedItems += 1
+                    treasureItemGroups[treasureItem.title, default: []].append(adjacentCell)
                 }
             }
         }
+        return successfullyPlacedItems
+    }
+    
+    
+    /// Finds the coordinates of an empty cell on the board.
+    /// A cell is considered empty if it does not contain any treasure.
+    /// - Returns: A tuple containing the row and column indices of an ampty cell.
+    private func findEmptyCell() -> (row: Int, column: Int)? {
+        while true {
+            let randomRow = Int.random(in: 0..<boardSize)
+            let randomColumn = Int.random(in: 0..<boardSize)
+            let randTile = tiles[randomRow][randomColumn]
+
+            if randTile.item == "" {
+                return (row: randomRow, column: randomColumn)
+            }
+        }
+    }
+    
+    
+    /// Finds the coordinates of an adjacent empty cell based on a given item group.
+    /// Searches for free spaces adjacent to the cells in the item group.
+    /// - Parameter itemGroup: The item group for which an adjacent empty cell is sought.
+    /// - Returns: A tuple containing the row and column indices of an adjacent empty cell, or nil if no adjacent cells found
+    private func findAdjacentEmptyCell(itemGroup: [(row: Int, column: Int)]) -> (row: Int, column: Int)? {
+        let directions: [(Int, Int)] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        let shuffledDirections = directions.shuffled()
+
+        for direction in shuffledDirections {
+            for cell in itemGroup {
+                let newRow = cell.row + direction.0
+                let newColumn = cell.column + direction.1
+
+                if (0..<boardSize).contains(newRow) && (0..<boardSize).contains(newColumn) && self[newRow, newColumn] == "" {
+                    return (row: newRow, column: newColumn)
+                }
+            }
+        }
+        return nil
     }
 }
--- Treasure_Hunting/GameView.swift
+++ Treasure_Hunting/GameView.swift
@@ -56,7 +56,7 @@ struct GameView: View {
             .padding()
         }
         .onAppear {
-            populateBoard()
+            board.populateBoard(treasureItems: treasureItems)
         }
     }
     
@@ -64,93 +64,6 @@ struct GameView: View {
         // Map your item names to system image names dynamically
         let systemImageName = UIImage(systemName: itemName)?.isSymbolImage ?? false ? itemName : "questionmark"
         return systemImageName
-    }
-    
-    private func populateBoard() {
-        let totalItems = treasureItems.reduce(0) { $0 + $1.count }
-        var successfullyPlacedItems = 0
-
-        while (successfullyPlacedItems != totalItems) {
-            clearBoard()
-            successfullyPlacedItems = placeItemsOnBoard()
-        }
-    }
-    
-    private func placeItemsOnBoard() -> Int {
-        var successfullyPlacedItems = 0
-        for treasureItem in treasureItems {
-            if let cell = findEmptyCell(board: board) {
-                board[cell.row, cell.column] = treasureItem.title
-                successfullyPlacedItems += 1
-                
-                if board.itemGroups[treasureItem.title] == nil {
-                    board.itemGroups[treasureItem.title] = [cell]
-                } else {
-                    board.itemGroups[treasureItem.title]?.append(cell)
-                }
-            }
-            
-            for _ in 2...treasureItem.count {
-                if let adjacentCell = findAdjacentEmptyCell(board: board, itemGroup: board.itemGroups[treasureItem.title] ?? []) {
-                    board[adjacentCell.row, adjacentCell.column] = treasureItem.title
-                    successfullyPlacedItems += 1
-                    
-                    // Add the adjacent cell to the item group
-                    if board.itemGroups[treasureItem.title] == nil {
-                        board.itemGroups[treasureItem.title] = [adjacentCell]
-                    } else {
-                        board.itemGroups[treasureItem.title]?.append(adjacentCell)
-                    }
-                }
-            }
-        }
-        return successfullyPlacedItems
-    }
-    
-    private func findAdjacentEmptyCell(board: Board, itemGroup: [(row: Int, column: Int)]) -> (row: Int, column: Int)? {
-        // Define the possible directions (up, down, left, right)
-        let directions: [(Int, Int)] = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-        // Shuffle the directions to start the search from a random direction
-        let shuffledDirections = directions.shuffled()
-
-        for direction in shuffledDirections {
-            for cell in itemGroup {
-                let newRow = cell.row + direction.0
-                let newColumn = cell.column + direction.1
-
-                // Check if the new position is within the board boundaries
-                if newRow >= 0 && newRow < board.boardSize && newColumn >= 0 && newColumn < board.boardSize {
-                    // Check if the adjacent cell is empty
-                    if board[newRow, newColumn] == "" {
-                        return (row: newRow, column: newColumn)
-                    }
-                }
-            }
-        }
-
-        // No empty adjacent cells found
-        return nil
-    }
-    
-    private func findEmptyCell(board: Board) -> (row: Int, column: Int)? {
-        while true {
-            let randomRow = Int.random(in: 0..<board.boardSize)
-            let randomColumn = Int.random(in: 0..<board.boardSize)
-            let randTile = board.tiles[randomRow][randomColumn]
-
-            if randTile.item == "" {
-                return (row: randomRow, column: randomColumn)
-            }
-        }
-    }
-    
-    private func clearBoard() {
-        for row in 0..<board.boardSize {
-            for col in 0..<board.boardSize {
-                board[row, col] = nil
-            }
-        }
     }
 }
 
