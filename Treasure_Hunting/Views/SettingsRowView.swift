b78a23cd0c62233b5e231a46dc4fd507405f9d4d
Ethan Portman <c0523782@online.camosun.ca>
2024-03-10 12:25:50 -0700

    Added documentation

--- Treasure_Hunting/Views/SettingsRowView.swift
+++ Treasure_Hunting/Views/SettingsRowView.swift
@@ -1,24 +1,26 @@
 import SwiftUI
 import os.log
 
+/// A SwiftUI view representing a row for a treasure item in the settings.
+/// In this view the treasure item title and count can be modified
 struct SettingsRowView: View {
+    /// The treasure item associated with this row.
     @Bindable var treasureItem: TreasureItem
+    /// The binding for the total number of items.
     @Binding var totalNumItems: Int
+    /// The logger instance for logging messages.
     private let logger = Logger()
-
+    
     var body: some View {
         HStack(spacing: 10) {
-            TextField("", text:
-                Binding(
-                    get: { treasureItem.title },
-                    set: { newValue in treasureItem.title = newValue }
-                )
-            )
-            
+            // TextField for editing the title of the treasure item.
+            TextField("", text: $treasureItem.title)
             Spacer()
             
+            // Display the count of the treasure item.
             Text("\(treasureItem.count)")
-
+            
+            // Stepper for adjusting the count of the treasure item.
             Stepper(
                 "",
                 onIncrement: {
