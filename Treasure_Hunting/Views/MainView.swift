751aff2cfa119eafeb787cb4c5e3309a75d4d7da
Ethan Portman <c0523782@online.camosun.ca>
2024-03-15 11:31:30 -0700

    finish

--- Treasure_Hunting/Views/MainView.swift
+++ Treasure_Hunting/Views/MainView.swift
@@ -2,8 +2,8 @@ import SwiftUI
 import SwiftData
 
 /// The main view of the Treasure Hunting app
-/// Displays tabs to switch betweent the Game View and the Settings View
-/// GameView is where the game is played, SettingsView is where treasures are configured
+/// Displays tabs to switch betweent the Game View and the Settings View.
+/// GameView is where the game is played, SettingsView is where treasures are configured.
 struct MainView: View {
     var body: some View {
         TabView {
